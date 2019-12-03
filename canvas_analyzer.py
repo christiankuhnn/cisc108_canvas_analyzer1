093e2553ced7933a04c7ffb912496fa74b1494c5
packrunner <kuhnc@udel.edu>
2019-12-03 10:52:31 -0500

    add submission

--- canvas_analyzer.py
+++ canvas_analyzer.py
@@ -11,9 +11,15 @@ To test your current solution, run the `test_my_solution.py` file.
 Refer to the instructions on Canvas for more information.
 
 "I have neither given nor received help on this assignment."
-author: YOUR NAME HERE
+author: Christian Kuhn
 """
 __version__ = 7
+
+
+
+
+
+
 
 # 1) main
 # 2) print_user_info
@@ -28,9 +34,68 @@ __version__ = 7
 
 # Keep any function tests inside this IF statement to ensure
 # that your `test_my_solution.py` does not execute it.
+import canvas_requests
+
+def main(user_id):
+    return canvas_requests.get_user(user_id)
+
+def print_user_info(user_dictionary):
+    print('Name: ' + user_dictionary['name'])
+    print('Title: ' + user_dictionary['title'])
+    print('Primary Email: ' + user_dictionary['login_id'])
+    print('Bio: ' + user_dictionary['bio'])
+
+def filter_available_courses(course_dictionaries):
+    filter_courses = []
+    for course in course_dictionaries:
+        if course['workflow_state'] == 'available':
+            filter_courses.append(course)
+    return filter_courses
+
+def print_courses(course_dictionaries):
+    for course in course_dictionaries:
+        print('\t'+ str(course['id']) + ": " + course['name'])
+
+def get_course_ids(course_dictionaries):
+    course_ids = []
+    for course in course_dictionaries:
+        course_ids.append(course['id'])
+    print()
+    return course_ids
+
+def choose_course(course_ids):
+    user_input = input("Enter a valid id")
+    user_input = int(user_input)
+    while int(user_input) not in course_ids:
+        user_input = int(input("Enter a valid id"))
+    return user_input
+
+def summarize_points(submissions):
+    possible = 0
+    points_obtained = 0
+    for submission in submissions:
+        if submission['score'] is not None:
+            points_possible = submission['assignment']['points_possible']
+            group_weight = submission['assignment']['group']['group_weight']
+            possible = possible + (points_possible * group_weight)
+            points_obtained = points_obtained + (submission['score'] * group_weight)
+        print('Points possible so far:' + str(possible))
+        print("Points obtained:" + str(points_obtained))
+        print("Current Grade:" + str(round((points_obtained/points_possible)* 100)))
+
+
+
+
+
+
 if __name__ == "__main__":
-    main('hermione')
-    # main('ron')
+    print_user_info(canvas_requests.get_user("hermione"))
+    filter_available_courses(canvas_requests.get_courses('hermione'))
+    print_courses(canvas_requests.get_courses('hermione'))
+    get_course_ids(canvas_rxxequests.get_courses('hermione'))
+    choose_course(canvas_requests.get_courses('hermione'))
+    summarize_points(canvas_requests.get_submissions('hermione'))
+    # main
     # main('harry')
     
     # https://community.canvaslms.com/docs/DOC-10806-4214724194
